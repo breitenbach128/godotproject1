18901448abf7f51605e672be14fa2afb8151fcbd
breitenbach128 <breitenbach128@hotmail.com>
2023-09-30 23:17:53 -0500

    updating zones

--- Zone.gd
+++ Zone.gd
@@ -1,10 +1,13 @@
 extends Sprite2D
 
 var over_zone:bool = false;
+var data:Dictionary = {};
+
+var player_trays = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	attach_player_trays();
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +19,6 @@ func _on_area_2d_mouse_entered():
 	over_zone = true;
 func _on_area_2d_mouse_exited():
 	over_zone = false;
+
+func attach_player_trays():
+	pass
